605e319ce76bd88bf1fe50d794c0d5d469dc589e
Seungeun727 <seun2230@gmail.com>
2022-11-16 12:53:03 +0900

    feat: Bcrypt를 이용하여 패스워드 암호화 추가, 상태 메시지 변경

--- routes/user.js
+++ routes/user.js
@@ -1,31 +1,28 @@
 const express = require('express');
 const router = express.Router();
 const pool = require('../db/index');
+const bcrypt = require('bcrypt');
 
 router.post('/register', async(req, res, next) => {
   const connection = await pool.getConnection(async conn => conn);
+  console.log(req.body);
   try {
-    let errorMessage = '';
-    let success = false;
-    const { name, email, id, password, passwordCheck } = req.body;
-    const values = [ 
-      id,
-      name,
-      password,
-      email,
-    ];
-
-    if(password !== passwordCheck) {
-      success = false;
-      errorMessage = '비밀번호가 일치하지 않습니다.';
-      return res.status(400).json({ errorMessage , success });
-    } else {
-      success = true;
+    const { name, email, id, passwordCheck } = req.body;
+    let plaintextPassword = req.body.password;
+  
+    if(plaintextPassword === passwordCheck) {
+      const saltRounds = await bcrypt.genSalt(10);
+      const encryptedPassword = await bcrypt.hash(plaintextPassword, saltRounds);
+      
       const sql = `INSERT INTO users (user_id, user_name, user_password, user_email) VALUES (?, ?, ?, ?)`;
-      await connection.query(sql, values);
+      const values = [ id, name, encryptedPassword, email];
+      
+      await connection.query(sql, values); 
       await connection.commit();
       connection.release();
-      return res.status(201).json({ successMessage: '회원가입에 성공했습니다.', success});
+      return res.status(201).json({ msg: '회원가입에 성공했습니다.', status: true});
+    } else {
+      return res.status(400).json({ msg: "비밀번호가 일치하지 않습니다.", status: false });
     }
   } catch(err) {
     connection.release();
